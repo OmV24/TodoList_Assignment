7502ac4474bd066e1f5c43b163403ae5eedd1034
Om Vichare <100900392+OmV24@users.noreply.github.com>
2023-01-02 19:20:01 +0530

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -22,12 +22,9 @@ defmodule Todolist.MixProject do
 
   defp deps do
     [
-      # {:plug, "~> 1.14.0"},
-      # {:cowboy, "~> 2.4"},
       {:plug_cowboy, "~> 2.5.2"},
       {:ecto_sql, "~> 3.0"},
       {:postgrex, "~> 0.16.5"},
-      {:crawly,"~> 0.8"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
